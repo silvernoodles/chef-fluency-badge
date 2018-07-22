699b03cc487bdf147c26ccc5f886342b3b5c5926
root <root@silvernoodles1.mylabserver.com>
2018-07-22 17:55:18 +0000

    Modified default.rb and added new recipe - motd.rb

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,15 +4,22 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 #
-#Install Apache 2.
+#Below logic will install Apache server based on whether the os on vm is rhel or debian based.
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platform_family'] == "debian"
+	package = "apache2"
+end
+
+
 package 'Apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
 #Enable Apache service and start at boot time.
 service 'Apache2' do
-	service_name 'httpd'
+	service_name package
 	action [:enable, :start]
 end
 
